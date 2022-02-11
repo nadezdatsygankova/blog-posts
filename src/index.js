55527acbf4868ace59276f0475c72d6401cdbdc7
nadezdatsygankova <nadiapritchina@gamil.com>
2022-02-10 20:28:26 -0500

    newReact

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import profile1 from './img/first.jpg'
+import profile2 from './img/second.jpg'
+import profile3 from './img/third.jpg'
+import SingleComment from './SingleComment';
+import UserCard from './UserCard';
+
+const App = () => {
+    return (
+        <div className='ui comments'>
+            <UserCard>
+                Hi. I live in Toronto
+            </UserCard>
+            <UserCard>
+                <SingleComment name='Alex'
+                    date='Today at 5 P.M.'
+                    text='it is amazing'
+                    picture={profile1} />
+            </UserCard>
+            <UserCard>
+                <SingleComment name='Jack'
+                    date='Today at 6 P.M.'
+                    text='Great job'
+                    picture={profile2} />
+            </UserCard>
+            <UserCard>
+                <SingleComment name='Sarah'
+                    date='Today at 7 P.M.'
+                    text='Thanks...'
+                    picture={profile3} />
+            </UserCard>
+        </div>
+    )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <App />,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    document.querySelector('#root')
+)
